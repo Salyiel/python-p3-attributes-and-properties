4fea5a66dc4ff6fb5c6ce7ce4c0944faecda6183
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-03 15:59:16 +0300

    Complete lab assessment

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,31 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name="", job=""):
+        self._name = ""
+        self._job = ""
+        self.name = name  # using the setter method
+        self.job = job  # using the setter method
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # Convert name to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
     pass
